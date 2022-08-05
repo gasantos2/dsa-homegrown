14f255cdd9273648ad0e6effd0179905ac68dfa9
gasantos2 <gasantos@redventures.com>
2022-08-05 10:47:53 -0300

    Create week01-homework.py

---
+++ week01-homework.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC #Introduction to Python
+
+# COMMAND ----------
+
+#This is how we print something to the screen using Python:
+print('Hello 2022 Accelerator!')
